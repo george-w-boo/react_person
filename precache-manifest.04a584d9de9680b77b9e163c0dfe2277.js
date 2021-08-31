4f95fda168d80fb8a238c9586e31549908dd6603
Mykhailo Ostapysko <mp4clips@gmail.com>
2021-08-31 17:40:55 +0300

    Updates

---
+++ precache-manifest.04a584d9de9680b77b9e163c0dfe2277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6390098b35f663714e776507e59157b6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7c10eff157428275f766",
+    "url": "./static/css/main.68fff2fd.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c10eff157428275f766",
+    "url": "./static/js/main.50d58d68.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
